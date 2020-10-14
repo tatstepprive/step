26cb0cf8f20cc042ad5eb006cedf408469c11d76
tatstep <tatstep@hotmail.com>
2020-10-14 21:02:52 +0200

    tatiana: search words js added

---
+++ apps/basic-learning-latin/latin-voc.js
@@ -0,0 +1,17 @@
+alert("connected");
+function filterTable(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#myTable tbody").rows;
+    
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }      
+    }
+}
+
+document.querySelector('#myInput').addEventListener('keyup', filterTable, false);
