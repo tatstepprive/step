dc97e45d69aa423dc549b8c1ee295c8540dd0299
tatstep <tatstep@hotmail.com>
2020-10-26 13:12:37 +0100

    tatiana: change color to random value

--- apps/minis/colorChanger.js
+++ apps/minis/colorChanger.js
@@ -2,10 +2,31 @@
 
 var myButton=document.querySelector("button");
 var myBody=document.querySelector("body");
+var colorInfo=document.querySelector("h1");
 
-myButton.addEventListener("click", function(){
+
+function blackWhite(){
 	console.log("button is clicked");  
 	myBody.style.background=myBody.style.background==="black"?"white":"black";
+	colorInfo.innerText=myBody.style.background==="white"?"white":"black";
+	colorInfo.style.color=myBody.style.background==="white"?"black":"white"
 	// other way is to add/remove = toogle the class name defined in css
 	//document.body.classList.toogle("purple"); 
-});
+}
+
+function randomColor(){
+	var myColor=getRandomColor();
+	console.log("button is clicked with picked color="+myColor);  
+	myBody.style.background=myBody.style.background=myColor;
+	colorInfo.innerText=myColor; 
+}
+
+
+function getRandomColor(){
+	var red=Math.floor(Math.random()*256);
+	var green=Math.floor(Math.random()*256);
+	var blue=Math.floor(Math.random()*256);
+	return "rgb("+red +", "+green+", "+ blue +")";
+}
+
+myButton.addEventListener("click", randomColor);
