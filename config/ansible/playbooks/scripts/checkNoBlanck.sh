6be3c44eefda0331b466aae117984e78bf90873f
tatstepprive <tatstep@hotmail.com>
2020-12-09 14:06:43 +0100

    disalbe ubuntu blanking script added

---
+++ config/ansible/playbooks/scripts/checkNoBlanck.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Check no blank 
+# we can see under Screen Saver, prefer blanking is set to no, and under DPMS, Standby, Suspend and Off are set to 0, and that DPMS is disabled.
+xset q
---
+++ config/ansible/playbooks/scripts/noblank.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Disable Screen Blanking in Linux
+xset dpms 0 0 0 && xset -dpms  && xset s off && xset s noblank
