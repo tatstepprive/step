d608f6da5145f2d22e02a99ea2890cbde9632f85
tatstepprive <tatstep@hotmail.com>
2021-01-29 13:30:49 +0100

    fix some params on export

--- notas/me/database/scripts/export.sh
+++ notas/me/database/scripts/export.sh
@@ -4,16 +4,16 @@
 FROM_DB=$PGDATABASE
 FROM_HOST=$PGHOST
 FROM_PASS=$PGPASSWORD
-PGUSER="postgres"
+FROM_USER=$PGUSER
 DUMP_DIR=/tmp/dumps
 
 while true ; do
   case "$1" in
-    --host) shift; FROM_DB=$1; shift;;
+    --host) shift; FROM_HOST=$1; shift;;
     --db) shift; FROM_DB=$1; shift;;
     --dr) shift; DUMP_DIR=$1; shift;;
-    --user) shift; PGUSER=$1; shift;;
-    --pass) shift; PGPASSWORD=$1; shift;;
+    --user) shift; FROM_USER=$1; shift;;
+    --pass) shift; FROM_PASS=$1; shift;;
     *) break;;
   esac
 done
@@ -25,7 +25,7 @@ LOG_FILE=${DUMP_DIR}/export_${FROM_DB}_${FROM_HOST}_${TIMESTAMP}.log
 
 ##Export data
 echo "[INFO] Start export data from db=$FROM_DB on host host=$PGHOST"
-PGPASSWORD=$FROM_PASS pg_dump -h $FROM_HOST -U $PGUSER --no-owner --no-acl -Fc -d $FROM_DB  -f $DUMP_FILE  --verbose 2>$LOG_FILE
+PGPASSWORD=$FROM_PASS pg_dump -h $FROM_HOST -U $FROM_USER --no-owner --no-acl -Fc -d $FROM_DB -f $DUMP_FILE  --verbose 2>$LOG_FILE
 echo "[INFO] End export data from db=$FROM_DB on host host=$PGHOST"
 echo "[INFO] DUMP FILE: $(ls -lh $DUMP_FILE)"
 
