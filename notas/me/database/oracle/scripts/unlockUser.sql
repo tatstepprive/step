4bc28b7841b0c8e1b9d1b24e5f6a1a42411e0319
tatstepprive <tatstep@hotmail.com>
2021-04-08 20:35:05 +0200

    profile info

--- notas/me/database/oracle/scripts/unlockUser.sql
+++ notas/me/database/oracle/scripts/unlockUser.sql
@@ -6,4 +6,8 @@ from v$containers;
 --start creating pluggable db (3 steps: be in root container, )
 alter session set container=pdb5;
 
+--unlock user
+alter user HR  account unlock;
+
+--unlock user and reset pass
 alter user HR identified by <new_pass> account unlock;
