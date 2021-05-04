4f770ab4e42aa76966e63ecdd4a50096a0db0535
tatstepprive <tatstep@hotmail.com>
2021-05-04 19:16:31 +0200

    controlfile nota

---
+++ notas/me/database/oracle/scripts/controlfile.sql
@@ -0,0 +1,8 @@
+show parameters control_files;
+shutdown immediate;
+-- move or copy control file (do no move or copy when db open)
+startup nomount;
+alter system set control_files="/data/control01.ctl, /data2/control01.clt, /data3/control03.ctl" scope=spfile;
+startup force;
+show parameters control_files;
+
