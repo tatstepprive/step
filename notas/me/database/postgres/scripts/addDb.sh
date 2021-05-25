46b5fd62cba356ac4c517ddedf73d7491f6c99e3
tatstepprive <tatstep@hotmail.com>
2021-05-25 20:05:07 +0200

    correct rights on public

--- notas/me/database/postgres/scripts/addDb.sh
+++ notas/me/database/postgres/scripts/addDb.sh
@@ -2,18 +2,25 @@
 echo "CONNECTING to -h $PGHOST -U postgres -p $PGPORT"
 echo "[INFO] Creating role \"$NEWUSER\""
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "CREATE ROLE \"$NEWUSER\" with login nocreaterole nocreatedb nosuperuser password '$SECRET';"
+
 echo "[INFO] Creating database \"$NEWDB\""
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "CREATE DATABASE \"$NEWDB\" OWNER \"$NEWUSER\";"
-echo "[INFO] Revoke connect on database \"$NEWDB\" from public"
+echo "[INFO] Revoke all on database \"$NEWDB\" from public"
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "REVOKE ALL ON DATABASE \"$NEWDB\" FROM PUBLIC;"
+echo "[INFO] Revoke all on schema public when connecting to database \"$NEWDB\""
+PGPASSWORD=$PGPASSWORD psql -h $PGHOST -d $NEWDB -U postgres -p $PGPORT -c "REVOKE ALL ON SCHEMA PUBLIC FROM PUBLIC;"
+
 echo "[INFO] Grant connect on database \"$NEWDB\" to \"$NEWUSER\""
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "GRANT CONNECT ON DATABASE \"$NEWDB\" TO \"$NEWUSER\";"
+echo "[INFO] Grant all on schema public when connecting to database \"$NEWDB\" to \"$NEWUSER\""
+PGPASSWORD=$PGPASSWORD psql -h $PGHOST -d $NEWDB -U postgres -p $PGPORT -c "GRANT ALL ON SCHEMA PUBLIC TO \"$NEWUSER\";"
+
 echo "[INFO] Creating role \"$NEWUSER_RO\""
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "CREATE ROLE \"$NEWUSER_RO\" with login nocreaterole nocreatedb nosuperuser password '$SECRET_RO';"
 echo "[INFO] Grant connect on database \"$NEWDB\" to \"$NEWUSER_RO\""
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "GRANT CONNECT ON DATABASE \"$NEWDB\" TO \"$NEWUSER_RO\";"
-echo "[INFO] Grant usage on schema public \"$NEWDB\" to \"$NEWUSER_RO\""
-PGPASSWORD=$PGPASSWORD psql -h $PGHOST -U postgres -p $PGPORT -c "GRANT USAGE ON SCHEMA PUBLIC TO \"$NEWUSER_RO\";"
+echo "[INFO] Grant usage on schema public when connecting on \"$NEWDB\" to \"$NEWUSER_RO\""
+PGPASSWORD=$PGPASSWORD psql -h $PGHOST -d $NEWDB -U postgres -p $PGPORT -c "GRANT USAGE ON SCHEMA PUBLIC TO \"$NEWUSER_RO\";"
 
 echo "[INFO] Grant select all tables ON DATABASE \"$NEWDB\" TO \"$NEWUSER_RO\""
 PGPASSWORD=$PGPASSWORD psql -h $PGHOST -d $NEWDB -U postgres -p $PGPORT -c "SET ROLE \"$NEWUSER\"; select current_user, session_user; GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO \"$NEWUSER_RO\";"
