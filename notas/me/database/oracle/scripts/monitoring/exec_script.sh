d3f8345aa0532c35d9745ce2bb9a8435fe7b4051
tatstepprive <tatstep@hotmail.com>
2021-05-27 18:46:59 +0200

    monitoring scripts added

---
+++ notas/me/database/oracle/scripts/monitoring/exec_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+MY_HOST=$ORAHOST
+MY_SID=$ORASID
+MY_USER=$ORAUSER
+MY_PASS=$ORAPASSWORD
+
+while true ; do
+  case "$1" in
+    --host) shift; MY_HOST=$1; shift;;
+    --sid) shift; MY_SID=$1; shift;;
+    --user) shift; MY_USER=$1; shift;;
+    --pass) shift; MY_PASS=$1; shift;;
+    *) break;;
+  esac
+done
+
+DB_CONNECT_STRING="(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=$MY_HOST)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=$MY_SID)))"
+sqlplus  "$MY_USER/$MY_PASS@$DB_CONNECT_STRING" <<EOF
+	@simple.sql
+EOF
