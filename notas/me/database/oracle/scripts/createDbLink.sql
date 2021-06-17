59e312e6d0c90d238ab4ebe3ce5571a63622a6e6
tatstepprive <tatstep@hotmail.com>
2021-06-17 17:41:49 +0200

    dblink expl

--- notas/me/database/oracle/scripts/createDbLink.sql
+++ notas/me/database/oracle/scripts/createDbLink.sql
@@ -1,5 +1,12 @@
 
-create database link <link_name> connect to <user> identified by <pass> using <connect_string_for_remote_db>;
+--create database link <link_name> connect to <user> identified by <pass> using <connect_string_for_remote_db>;
+CREATE [PUBLIC] DATABASE LINK MY_LINK
+    CONNECT TO remote_user IDENTIFIED BY password
+    USING '(DESCRIPTION=
+                (ADDRESS=(PROTOCOL=TCP)(HOST=oracledb.example.com)(PORT=1521))
+                (CONNECT_DATA=(SERVICE_NAME=service_name))
+            )';
+
 --connect_string_for_remote_db can be:
 --"(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=<hostname>)(PORT=<port>)))(CONNECT_DATA=(SERVICE_NAME=<service_name>)))"
 --"(ADDRESS=(PROTOCOL=TCP)(HOST=<hostname>)(PORT=<port>)))(CONNECT_DATA=(SERVICE_NAME=<service_name>))"
