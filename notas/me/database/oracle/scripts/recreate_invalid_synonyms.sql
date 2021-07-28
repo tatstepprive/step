a5e5c0d39fe137b04e6feff726f2a63f58b45f39
tatstepprive <tatstep@hotmail.com>
2021-07-28 16:49:24 +0200

    recreate invalid synonyms

---
+++ notas/me/database/oracle/scripts/recreate_invalid_synonyms.sql
@@ -0,0 +1,7 @@
+--check for invalid synonym and the target object exists
+select owner, synonym_name, table_owner, table_name
+from all_synonyms
+where synonym_name in  (select object_name from dba_objects where status='INVALID' and object_type='SYNONYM');
+and exists (select object_name from dba_objects where owner=table_owner and object_name=table_name);
+
+--create or replace synonym owner.synonym_name for table_owner.table_name;
