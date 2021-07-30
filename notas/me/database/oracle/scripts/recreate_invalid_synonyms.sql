feb0bb416af7cb4fafe3398e7723324fa5d6602c
tatstepprive <tatstep@hotmail.com>
2021-07-30 09:50:14 +0200

    notas for synonym creation

--- notas/me/database/oracle/scripts/recreate_invalid_synonyms.sql
+++ notas/me/database/oracle/scripts/recreate_invalid_synonyms.sql
@@ -4,4 +4,4 @@ from all_synonyms
 where synonym_name in  (select object_name from dba_objects where status='INVALID' and object_type='SYNONYM');
 and exists (select object_name from dba_objects where owner=table_owner and object_name=table_name);
 
---create or replace synonym owner.synonym_name for table_owner.table_name;
+--create or replace [public] synonym owner.synonym_name for table_owner.table_name;
