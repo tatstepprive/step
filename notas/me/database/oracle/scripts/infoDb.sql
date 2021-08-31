79fccc77503b6747fd9303f7fdd39613a3fa17ae
tatstepprive <tatstep@hotmail.com>
2021-08-31 22:44:41 +0200

    some notes

--- notas/me/database/oracle/scripts/infoDb.sql
+++ notas/me/database/oracle/scripts/infoDb.sql
@@ -9,3 +9,8 @@ select name from v$instance;
 
 --show db unique number (DBID=xxxxxxx where x=1-9)
 select dbid from v$database;
+
+--show connection info
+SELECT SYS_CONTEXT('USERENV','SERVER_HOST') FROM dual;
+SELECT host_name FROM v$instance;
+SELECT terminal, machine FROM v$session WHERE username = 'HR';
