db3d84e5f2a494fad72c8546050a69509673e7ca
tatstepprive <tatstep@hotmail.com>
2021-12-19 15:26:52 +0100

    resize redo

--- notas/me/database/oracle/roadmap/modify/redologConfigModify.sql
+++ notas/me/database/oracle/roadmap/modify/redologConfigModify.sql
@@ -1,4 +1,10 @@
 ------------------------------------------
+--Add group
+alter database add logfile
+     ('/u01/app/oracle/oradata/orcl/redo_g4m1.rdo',
+      '/u01/app/oracle/oradata/orcl/redo_g4m2.rdo')
+     size 50m;
+-------------------------------------------
 -- Add member to group (online operation: db open)
 alter database add logfile member
 'path'
@@ -18,3 +24,46 @@ alter system switch logfile;
 ALTER DATABASE RENAME FILE '/diska/logs/log1a.rdo' TO '/diskc/logs/log1c.rdo';
 --ALTER DATABASE OPEN; 
 ---------------------------------------------
+--drop redo group
+-- conditions: min 2 groups available, status group INACTIVE
+-- if OMF (oracle managed files) used: member files will be auto removed on OS
+-- if no OMF used: remove member files on OS self
+alter database drop logfile group 3;
+---------------------------------------------
+--drop member
+-- conditions: min 2 members in group  available, status group INACTIVE
+-- remove member file on OS self alfter sql command
+alter database drop logfile member '/diskc/logs/log1c.rdo';
+--------------------------------------------
+--Resize redo logs (online operation)
+--check current size
+select group#,members,status,bytes/1024/1024 as mb from v$log;
+--check location
+select group#,member from v$logfile;
+alter database add logfile
+     ('/u01/app/oracle/oradata/orcl/redo_g4m1.rdo',
+      '/u01/app/oracle/oradata/orcl/redo_g4m2.rdo')
+     SIZE 100m;
+alter database add logfile
+     ('/u01/app/oracle/oradata/orcl/redo_g5m1.rdo',
+      '/u01/app/oracle/oradata/orcl/redo_g5m2.rdo')
+     size 100m;
+alter database add logfile
+     ('/u01/app/oracle/oradata/orcl/redo_g6m1.rdo',
+      '/u01/app/oracle/oradata/orcl/redo_g6m2.rdo')
+     size 100m;
+-- drop group inactive, never drop active or current
+select group#,members,status,bytes/1024/1024 as mb from v$log;
+alter database drop logfile group 1;
+alter database drop logfile group 2;
+
+-- take in use new added group
+alter system switch logfile;
+
+--get rid from active group 
+alter system checkpoint;
+alter database drop logfile group 3;
+
+--test all new redo log groups by cycling
+alter system switch logfile;
+-----------------------------------------
