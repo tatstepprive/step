c6a6301381544ee8e039322123d0a292465b573e
tatstepprive <tatstep@hotmail.com>
2022-01-08 20:57:17 +0100

    extra info about open cursors

--- notas/me/database/oracle/scripts/monitorSendMailViaDb.sql
+++ notas/me/database/oracle/scripts/monitorSendMailViaDb.sql
@@ -491,5 +491,6 @@ end;
 select * from sys.max_cursor_monitor order by value desc;
 
 select * from v$open_cursor where user_name='HR' and sid=88;
+select * from v$open_cursor where user_name='HR' and sid=88 and cursor_type like 'OPEN%' ;
 --------------------------
 
