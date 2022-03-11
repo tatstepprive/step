57d0d0fe706f268d0df6ba4ddf01cfa22557e236
tatstepprive <tatstep@hotmail.com>
2022-03-11 08:00:28 +0100

    rename tbs

--- notas/me/database/oracle/scripts/alterTablespace.sql
+++ notas/me/database/oracle/scripts/alterTablespace.sql
@@ -1,6 +1,17 @@
+--RENAME 
+--rename tablespace
+alter tablespace ts1 rename to ts2;
+--rename datafile online (zero down time)
+--alter database move datafile 'path' to 'newpath';
+-- the new path should exist. First create directory /u01/app/oracle/oradata/ORCL/pdb1/data/ 
+alter database move datafile '/u01/app/oracle/oradata/ORCL/pdb1/ts1_02.dbf' to '/u01/app/oracle/oradata/ORCL/pdb1/data/ts1_002.dbf';
+--Queries and dml, ddl operations can be performed when renaming/moving datafiles in tablespace
+-- if REUSE option is done, the existing file will be overwritten if it's exists
+-- if KEEP option is done (not allowed for oracle managed files OMF), the old file will be kept
+-----------------------------------------------
+--READ ONLY
+--tablespace read write or read only (ro= no DML, but drop can)
 -- set tablespace read only
-alter tablespace ts1 read only;
--- change something in this tablespace
 alter tablespace ts1 read only;
 
 select * from dba_users where default_tablespace='TS1';
@@ -19,6 +30,12 @@ insert into molly.city values (1,'brussels');
 --set tablespace read write
 alter tablespace ts1 read write;
 ------------------------------------------------
+--ONLINE OFFLINE
+-- if tbs is offline - can not be used
+alter tablespace ts1 offline;
+alter tablespace ts1 online;
+------------------------------------------------
+--RESIZE
 -- Resize tablespace methode1: resize file (now 100MB, set 200MB) (if tablespace ts1 consist from 1 file)
 -- show size before
 select dbms_metadata.get_ddl('TABLESPACE', 'TS1') from dual;
@@ -27,25 +44,10 @@ alter database datafile '/u01/app/oracle/oradata/ORCL/pdb1/xxx/xxx.dbf' resize  
 -- show size after
 select dbms_metadata.get_ddl('TABLESPACE', 'TS1') from dual;
 -- Resize tablespace methode2: add extra file to tablespace ts1 -> tablespace size is sum of all files in this tablespace
+-- alter tablespace ts1 add datafile 'path' size nK|M|G;
 alter tablespace ts1 
 add datafile '/u01/app/oracle/oradata/ORCL/pdb1/ts1_02.dbf' size 10M;
 --check new file added
 select * from v$datafile;
 --------------------------------------------------
--- Move datafiles in tablespace
--- the path should exist. First create directory /u01/app/oracle/oradata/ORCL/pdb1/data/ 
-alter database move datafile
-'/u01/app/oracle/oradata/ORCL/pdb1/ts1_02.dbf'
-to
-'/u01/app/oracle/oradata/ORCL/pdb1/data/ts1_02.dbf';
 
---Rename datafile in tablespace
-alter database move datafile
-'/u01/app/oracle/oradata/ORCL/pdb1/data/ts1_02.dbf'
-to
-'/u01/app/oracle/oradata/ORCL/pdb1/data/ts1_002.dbf';
-
---Queries and dml, ddl operations can be performed when renaming/moving datafiles in tablespace
--- if REUSE option is done, the existing file will be overwritten if it's exists
--- if KEEP option is done (not allowed for oracle managed files OMF), the old file will be kept 
------------------------------------------------------
