1ddb5378433d0c1b36e8c7147ea1532d85e2a3fe
tatstepprive <tatstep@hotmail.com>
2022-05-08 09:32:42 +0200

    archive log dest

---
+++ notas/me/database/oracle/roadmap/show/showArchiveLogLocation.sql
@@ -0,0 +1,12 @@
+--Show archive log location
+archive log list
+--see Archive Destination USE_RECOVERY_FILE_DEST
+show parameter RECOVERY_FILE_DEST;
+--value=<path>
+
+--The archive destination can be set with parameter log_archive_dest_n where n=number
+show parameter log_archive_dest;
+
+--show archive dest
+select * from v$archive_dest;
+
